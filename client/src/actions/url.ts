2d633abb009a0abf177cb908b520df1d767c4da8
Aragor70 <mikey.prus@gmail.com>
2022-04-24 11:26:59 +0000

    Client update: Include url

--- client/src/actions/url.ts
+++ client/src/actions/url.ts
@@ -9,11 +9,11 @@ export const getURLs = async() => {
 
     try {
         
-        const res = await axios.get('/api/urls')
+        const res = await axios.get('https://shortnister.herokuapp.com/api/urls')
         
         return res.data
 
-    } catch (err) {
+    } catch (err: any) {
         console.log(err.message)
         
     }
@@ -23,11 +23,11 @@ export const getUrl = async(code: string) => {
 
     try {
 
-        const res = await axios.get(`/api/urls/${code}`)
+        const res = await axios.get(`https://shortnister.herokuapp.com/api/urls/${code}`)
         
         return res.data
 
-    } catch (err) {
+    } catch (err: any) {
         console.log(err.message)
         
     }
@@ -42,12 +42,12 @@ export const postUrl = async(formData: formType, history: any) => {
     }
     try {
 
-        const res = await axios.post('/api/urls', formData, config)
+        const res = await axios.post('https://shortnister.herokuapp.com/api/urls', formData, config)
         
         history.push(`/${res.data.urlAddress.urlCode}/overview`)
         return res
 
-    } catch (err) {
+    } catch (err: any) {
         console.log(err.message)
         
         
